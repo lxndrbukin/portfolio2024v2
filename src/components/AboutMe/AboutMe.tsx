6bead69a7da3016f322a3686dc2fb95f13d57fd8
lxndrbukin <alex.bukin@hotmail.com>
2023-12-20 22:57:45 +0000

    Working on adding actual content instead of placeholders

--- src/components/AboutMe/AboutMe.tsx
+++ src/components/AboutMe/AboutMe.tsx
@@ -6,22 +6,24 @@ export class AboutMe extends Component {
     return (
       <div id='about' className='about'>
         <p>
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Eius
-          necessitatibus quod iusto laborum, libero pariatur modi vitae amet
-          odit commodi in dolore. Doloremque quod necessitatibus, nam
-          reprehenderit illo rerum ipsam.
+          Even when I was just a teenager I was always interested in Tech and
+          Software Development. I've always tried to play around with HTML and
+          CSS but never actually tried learning Web Development. I was mostly
+          having fun with pre-made templates and just tried to play around with
+          the <b>HTML</b> and <b>CSS</b> code when I was younger.
         </p>
         <p>
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Eius
-          necessitatibus quod iusto laborum, libero pariatur modi vitae amet
-          odit commodi in dolore. Doloremque quod necessitatibus, nam
-          reprehenderit illo rerum ipsam.
+          In 2020 I finally decided to give it a go and began learning Web
+          Development. Slowly but surely I've been gaining knowledge of{' '}
+          <b>HTML</b>, <b>CSS</b> and <b>JavaScript</b>.
         </p>
         <p>
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Eius
-          necessitatibus quod iusto laborum, libero pariatur modi vitae amet
-          odit commodi in dolore. Doloremque quod necessitatibus, nam
-          reprehenderit illo rerum ipsam.
+          Today I'm finally not just playing around with pre-made templates but
+          actually creating projects from scratch using <b>JavaScript</b>,{' '}
+          <b>TypeScript</b> and libraries/frameworks like <b>React</b> on the
+          frontend and <b>Express</b> on the backend. This is only the beginning
+          of my journey and I still have a lot to learn but I can't wait to see
+          what awaits me ahead.
         </p>
       </div>
     );
